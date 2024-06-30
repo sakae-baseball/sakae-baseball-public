48063cc8c292137ecc6a181ade2fbabe2cc86c12
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2024-06-30 05:40:02 +0000

    GA4

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,12 @@
   ga('create', 'UA-90717718-1', 'auto');
   ga('send', 'pageview');
 
+  // GA4 スクリプト
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-W7E5WF4QNH');
 
 
 
